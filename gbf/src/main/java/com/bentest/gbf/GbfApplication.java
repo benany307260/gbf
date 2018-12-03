1adbfd00f1afe991c96633d3cd3f204c46eb8795
黄志斌 <307260621@qq.com>
2018-12-03 23:30:03 +0800

    gdk

---
+++ gbf/src/main/java/com/bentest/gbf/GbfApplication.java
@@ -0,0 +1,12 @@
+package com.bentest.gbf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbfApplication.class, args);
+	}
+}
