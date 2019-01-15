0b67b5c418daa7686237b4a4eb979e9de1f001d6
soap <soap@34.39.17.218.broad.sz.gd.dynamic.163data.com.cn>
2019-01-15 20:50:56 +0800

    增加mybatics

--- src/main/java/com/bentest/gbf/GbfApplication.java
+++ src/main/java/com/bentest/gbf/GbfApplication.java
@@ -1,9 +1,11 @@
 package com.bentest.gbf;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.bentest.gbf.domain.mapper")
 public class GbfApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/bentest/gbf/domain/entity/User.java
@@ -0,0 +1,11 @@
+package com.bentest.gbf.domain.entity;
+
+public class User {
+	private String userName;
+	
+	private String password;
+	
+	private String slat;
+	
+	private String nickName;
+}
